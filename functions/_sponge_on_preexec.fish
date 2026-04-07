e63e4c3fc483ba36b30c3c2906141a7e62dc40da
Justin Su <injustsu@gmail.com>
2026-04-07 05:22:22 -0400

    Remove redundant line continuation

--- functions/_sponge_on_preexec.fish
+++ functions/_sponge_on_preexec.fish
@@ -3,8 +3,8 @@ function _sponge_on_preexec --on-event=fish_preexec --argument-names command
 
     set -g _sponge_current_command $command
 
-    builtin history search --case-sensitive --exact --max=1 --null $command \
-        | read --local --null found_entries
+    builtin history search --case-sensitive --exact --max=1 --null $command |
+        read --local --null found_entries
 
     # If a command is in the history and in the queue, ignore it, like if it wasn’t in the history
     if test (count $found_entries) -ne 0; and not contains $command $_sponge_queue
